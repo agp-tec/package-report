48e9dad59b1dc7c5ef3565c1c2842a5f0e5fb34d
francisco <francisco.gomes@ticketmais.com.br>
2020-10-01 14:05:40 -0300

    Comand php artisan make:report

--- src/Agp/Report/AgpReportServiceProvider.php
+++ src/Agp/Report/AgpReportServiceProvider.php
@@ -2,12 +2,18 @@
 
 namespace Agp\Report;
 
+use Agp\Report\Console\Commands\MakeCommand;
 use Illuminate\Support\ServiceProvider;
 
 class AgpReportServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                MakeCommand::class,
+            ]);
+        }
 //        $this->loadRoutesFrom(__DIR__ . '/Routes/web.php');
     }
 
---
+++ src/Agp/Report/Console/Commands/MakeCommand.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Agp\Report\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+
+class MakeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:report {model}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Cria uma classe de Report.';
+    /**
+     * @var Filesystem
+     */
+    protected $files;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(Filesystem $files)
+    {
+        $this->files = $files;
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     */
+    public function handle()
+    {
+        $model = $this->argument('model');
+        if ($model == '')
+            return $this->error('Nome da model entity é inválido.');
+
+        $contents =
+            '<?php
+
+
+namespace App\Reports;
+
+
+use Agp\Report\ReportTotalizador;
+use App\Model\Entity\\' . $model . ';
+
+class ' . $model . 'Report extends \Agp\Report\Report
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        //SQL inicial
+        $this->queryBuilder = function () {
+            return ' . $model . '::query();
+        };
+
+        //Faz o bind dos campos da entidade (fillable)
+        $this->setModel(new ' . $model . '());
+
+        //$this->getColumnByName(\'id\')->filter->set(\'int\',\'=\');
+        //$this->getColumnByName(\'id\')->totalizador = new ReportTotalizador(\'count\');
+    }
+}';
+        if ($this->confirm('Do you wish to create ' . $model . 'Report file?')) {
+            $file = "${model}Report.php";
+            $path = app_path();
+
+            $file = $path . "/Reports/$file";
+            $composerDir = $path . "/Reports";
+
+            if ($this->files->isDirectory($composerDir)) {
+                if ($this->files->isFile($file))
+                    return $this->error($model . 'Report Already exists!');
+
+                if (!$this->files->put($file, $contents))
+                    return $this->error('Something went wrong!');
+                $this->info("{$model}Report generated!");
+            } else {
+                $this->files->makeDirectory($composerDir, 0775, true, true);
+
+                if (!$this->files->put($file, $contents))
+                    return $this->error('Something went wrong!');
+                $this->info("{$model}Report generated!");
+            }
+        }
+    }
+}
