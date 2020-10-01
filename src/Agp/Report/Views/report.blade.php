14820d5d106547d8d717e8d5da593f7fad882101
francisco <francisco.gomes@ticketmais.com.br>
2020-10-01 17:25:54 -0300

    Melhorias

--- src/Agp/Report/Views/report.blade.php
+++ src/Agp/Report/Views/report.blade.php
@@ -10,8 +10,8 @@
     <div class="card-body py-0" id="transacoes">
         <div class="collapse mb-5" id="collapseOpcoes">
             <div class="card card-body">
-                <form method="get">
-                    <div class="row border">
+                <div class="row">
+                    <form method="get">
                         @foreach($report->columns as $column)
                             @if($column->filter->tipo != '')
                                 <div class="col-md-3 mr-2">
@@ -20,20 +20,21 @@
                                 </div>
                             @endif
                         @endforeach
-                        <div class="col-md-1 border align-items-center">
+                        <div class="col-md-1">
                             <button class="btn btn-primary font-weight-bolder font-size-sm" type="submit">Filtrar
                             </button>
                         </div>
-
-                        <div class="col-md-1">
-                            <form method="get">
-                                <input class="form-control" type="hidden" name="export" value="1">
-                                <button class="btn btn-success font-weight-bolder font-size-sm" type="submit">Download
-                                </button>
-                            </form>
-                        </div>
+                    </form>
+                </div>
+                <div class="row">
+                    <div class="col-md-2 text-right align-content-end justify-content-end">
+                        <form method="get">
+                            <input class="form-control" type="hidden" name="export" value="1">
+                            <button class="btn btn-success font-weight-bolder font-size-sm" type="submit">Download
+                            </button>
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
         <table class='table table-head-custom table-head-bg table-borderless table-vertical-center'>
@@ -68,7 +69,7 @@
             </tbody>
         </table>
         <div class="d-flex justify-content-center mt-5">
-            <table class='table table-bordered'>
+            <table class='table table-borderless'>
                 <thead>
                 <tr>
                     @foreach($report->columns as $column)
