8bb1bb90e4c6285bc8bcdbd9b10ae6b006c314ab
francisco <francisco.gomes@ticketmais.com.br>
2021-06-09 10:53:55 -0300

    Melhoria Layout switch

--- src/Agp/Report/Views/input/switch.blade.php
+++ src/Agp/Report/Views/input/switch.blade.php
@@ -1,10 +1,10 @@
-<div class="d-flex flex-column justify-content-end mb-2 switch-report">
+<div class="d-flex flex-column justify-content-end mb-2 div-switch-report">
     <span class="text-muted mt-3 font-weight-bold font-size-sm">
         {{ $inputTitle }}
     </span>
     <span class="switch switch-md switch-icon">
         <label>
-            <input name="{{ $inputName }}" type="checkbox">
+            <input class="input-switch-report" name="{{ $inputName }}" type="checkbox">
             <span></span>
         </label>
     </span>
